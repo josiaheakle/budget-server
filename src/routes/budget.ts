ffe3ed2f6a3b4615ef124d8ca93164377056e364
henryotoole (dev server) <josiaheakle@gmail.com>
2022-07-08 00:34:49 +0000

    moving to ubuntu

--- src/routes/budget.ts
+++ src/routes/budget.ts
@@ -1,7 +1,9 @@
 import Express from "express";
+import { isUserLoggedIn } from "../middleware/isUserLoggedIn";
 
 const router = Express.Router();
 
+router.use(isUserLoggedIn);
 router.get("/:budgetId", (req, res, next) => {});
 
 export { router };
